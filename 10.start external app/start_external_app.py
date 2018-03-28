e6c91e5869bcbec4d4c67c736630321a139817d0
Healwy <982973349@qq.com>
2018-03-29 00:12:01 +0800

    start external app

---
+++ 10.start external app/start_external_app.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+
+import subprocess
+import os
+
+#打开记事本
+# os.system('notepad')
+
+
+#打开计算器
+#os.system('calc')
+
+#打开网易云音乐
+#os.system(r"C:\Program Files (x86)\Netease\CloudMusic\cloudmusic.exe")
+#subprocess.call(r"C:\Program Files (x86)\Netease\CloudMusic\cloudmusic.exe")
+
+#打开相应的文件使用默认程序
+#os.system('test.txt')
+#subprocess.run(['start','test.txt'], shell=True)
+
+#打开网页
+# import webbrowser
+# webbrowser.open("www.baidu.com")
